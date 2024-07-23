2a236923d638b5acecdb3ea81e5ec972a6ddfdd5
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-23 16:02:40 -0300

    create command line runner

--- src/main/java/com/tidz/aopdemo/AopdemoApplication.java
+++ src/main/java/com/tidz/aopdemo/AopdemoApplication.java
@@ -1,13 +1,22 @@
 package com.tidz.aopdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AopdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run(String[] args) {
+		return runner -> {
+			System.out.println("Hello World!");
+		};
 	}
 
 }
